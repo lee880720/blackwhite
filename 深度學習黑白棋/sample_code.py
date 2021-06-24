36449c4bdbeda80606ed56582704811aa3ac3222
lee880720 <artttlee@gmail.com>
2021-06-24 15:50:24 +0800

    ㄓ
    5

---
+++ 深度學習黑白棋/sample_code.py
@@ -0,0 +1,12 @@
+from AIGamePlatform import Othello
+from othello.bots.Random import BOT
+import time
+
+app=Othello() # 會開啟瀏覽器登入Google Account，目前只接受@mail1.ncnu.edu.tw及@mail.ncnu.edu.tw
+bot=BOT()
+
+@app.competition(competition_id='練習賽')
+def _callback_(board, color): # 函數名稱可以自訂，board是當前盤面，color代表黑子或白子
+    time.sleep(0.5)
+    return bot.getAction(board, color) # 回傳要落子的座標
+
